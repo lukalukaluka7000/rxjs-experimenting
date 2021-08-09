9ae59e8aea4b0e3f58275b0328bb14bb7379e591
lukalukaluka7000 <luka.busha-st3@hotmail.com>
2021-08-09 12:18:04 +0200

    good way #2

--- src/app/from-articles/toggling-state/toggling-state.component.ts
+++ src/app/from-articles/toggling-state/toggling-state.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { merge, fromEvent, Observable, of } from 'rxjs';
+import { mapTo, map, startWith, tap, filter } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-toggling-state',
   templateUrl: './toggling-state.component.html',
   styleUrls: ['./toggling-state.component.css']
 })
-export class TogglingStateComponent implements OnInit {
-  query = '';
-  isSearchInputVisible = false;
-
-  showSearchInput(event: MouseEvent) {
-    event.stopPropagation();
-    this.isSearchInputVisible = true;
+export class TogglingStateComponent implements AfterViewInit {
+  tekst = new FormControl(['']);
+  @ViewChild('btn') buttonRef: ElementRef<HTMLInputElement>;
+  
+  isSearchInputVisible$ : Observable<boolean> = of(false);
+  
+  ngAfterViewInit(): void {
+    //merge vs CombineLatest return Operators which is not useful in this siutation
+    this.isSearchInputVisible$ = merge(
+      fromEvent(this.buttonRef.nativeElement, 'click').pipe( tap(e=>e.stopPropagation()), mapTo(true) ),
+      fromEvent(document.body, 'click').pipe( filter(() => this.tekst.value === '' ), mapTo(false) )
+    ).pipe(
+      startWith(false),
+      tap(() => console.log(this.tekst.value))
+    ); 
   }
-
-  @HostListener('document:click')
-  hideSearchInput() {
-    if (this.query === '') {
-      this.isSearchInputVisible = false;
-    }
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
