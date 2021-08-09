9f4f03fe015a4d8969524357f32eaa97219307d5
lukalukaluka7000 <luka.busha-st3@hotmail.com>
2021-08-09 17:41:59 +0200

    fix err #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SelectFilterComponent } from './from-articles/select-filter/select-filter.component';
 import { TogglingStateComponent } from './from-articles/toggling-state/toggling-state.component';
-import { ImperativeTimeComponent } from './imperative-time/imperative-time.component';
+import { ImperativeTimeComponent } from './from-articles/imperative-time/imperative-time.component';
 @NgModule({
   declarations: [
     AppComponent,
